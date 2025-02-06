2f4b21bd6ee339a658c86be88c5074da998ef607
Kishan Bapodra <kishanbapodra2002@gmail.com>
2025-02-07 02:21:44 +0400

    Add SearchResults page

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router";
+import SearchResults from "./components/SearchResults.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/search-results" element={<App />} />
+        <Route path="/search-results" element={<SearchResults />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>
